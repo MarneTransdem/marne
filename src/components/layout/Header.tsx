97ed90fbd00d0b7c140fc0bf1377d16908162ace
Marne Transdem <contact@marnetransdem.com>
2026-09-20 11:32:02 +0200

    Compact homepage header and preserve flush hero alignment

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -52,12 +52,12 @@ export const Header: React.FC = () => {
     <>
       <header 
         className={`${location.pathname === '/' ? 'home-site-header ' : ''}fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
-          isScrolled ? 'glass py-3' : `bg-transparent pt-5 ${location.pathname === '/' ? 'pb-5 md:py-8' : 'pb-8 md:pt-8 md:pb-12'}`
+          isScrolled ? 'glass py-3' : location.pathname === '/' ? 'bg-transparent py-3' : 'bg-transparent pt-5 pb-8 md:pt-8 md:pb-12'
         }`}
       >
         <div className="container mx-auto px-4 md:px-6 flex items-center justify-between">
           <Link to="/" className="shrink-0" onClick={closeMenu}>
-            <Logo height="h-14 md:h-16" />
+            <Logo height={location.pathname === '/' ? 'h-12 md:h-14' : 'h-14 md:h-16'} />
           </Link>
 
           {/* Desktop Nav */}
